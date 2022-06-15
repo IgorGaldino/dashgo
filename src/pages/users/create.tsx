660bd1b9b0c591466da4a159ab4333bf1b81f97c
Igor Galdino <igormgaldino@gmail.com>
2022-06-14 22:21:45 -0300

    Adjustments on routes

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,14 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+import Link from "next/link";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -11,7 +21,9 @@ export default function CreateUser() {
         <Sidebar />
 
         <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
-          <Heading size="lg" fontWeight="normal">Criar usuário</Heading>
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
@@ -22,13 +34,21 @@ export default function CreateUser() {
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" type="password" label="Senha" />
-              <Input name="password_confirmation" type="password" label="Confirmação da Senha" />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da Senha"
+              />
             </SimpleGrid>
           </VStack>
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
             </HStack>
           </Flex>
