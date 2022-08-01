60735e7dc61f5f61093c41808789943dd52de3eb
Igor Galdino <igormgaldino@gmail.com>
2022-08-01 19:52:27 -0300

    Format user list in the table

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { theme } from "../styles/theme";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -20,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
---
+++ src/services/hooks/useUsers.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../api";
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
+
+export async function getUsers (): Promise<User[]> {
+  const { data } = await api.get("users");
+  const users = data.users.map(user => ({
+    ...user,
+    createdAt: new Date(user.createdAt).toLocaleString("pt-BR", {
+      day: "2-digit",
+      month: "long",
+      year: "numeric"
+    })
+  }))
+  return users;
+}
+
+export function useUsers () {
+  return useQuery(["users"], getUsers , {
+    staleTime: 1000 * 5 // 5 seconds
+  });
+}
